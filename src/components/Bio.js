f3847ba9501acc6cf8a2f439f6078a691529038e
craig t smith <craigtsmith@users.noreply.github.com>
2018-05-22 00:26:17 +0100

    first commit

---
+++ src/components/Bio.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`craig t smith`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          though / written by me, <strong>craig t smith</strong> how very nice to meet you. <br/>
+          <a href="https://twitter.com/craig_tsmith">
+            i sometimes tweet
+          </a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/hello-world/index.md
@@ -0,0 +1,28 @@
+---
+title: hello world
+date: "2018-05-22T22:12:03.284Z"
+---
+
+i move to amsterdam in 3 weeks.  
+infact it's 2 weeks 6 days, 23 hours and 50 minutes.
+
+not that i'm counting or anything.
+
+except i am counting, and making lists, and freaking out, and getting 
+hella excited and freaking out and cleaning things and freaking out and 
+ticking things off lists, and getting excited and keeping busy and 
+freaking out, and packing and searching for a house and freaking out 
+and and and really i should be in bed.
+
+but i'm not.  
+because i'm super excited.  
+(i'm freaking out.)
+
+instead i setup this blog - because actually i _am_ super excited, and
+i want somewhere to write about the adventure me & stuart are about to
+embark on.
+
+i'll write about the whys and wheres and what have yous another time, 
+for now i'll just say...
+
+"hello world" 
